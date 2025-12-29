8028c4160083f16075aecb7d26a80504465c8913
sarmatadylov <sarmatadylov05@gmail.com>
2025-12-29 16:04:02 +0600

    фриланс ок

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-import product.views
+from jobs.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', product.views.index), 
+    path('admin/', admin.site.urls), 
+    path('', index, name='home'),
+    
 ]
